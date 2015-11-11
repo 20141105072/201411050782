3fd9748eb1eb7e15b46b8ada391a663d7c8ac7ab
20141105072 <2196690899@qq.com>
2015-11-11 20:11:41 +0800

    指针

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  20141105072
+//
+//  Created by 20141105072 on 15/11/11.
+//  Copyright © 2015年 20141105072. All rights reserved.
+//
+
+#include <iostream>
+#define N 5
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x  &a=%x  &a=%x  p=%x  *p=%x\n",a,&a,&p,p,*p);
+    return 0;
+}
